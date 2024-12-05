72ddba2c76352dcc26fbbfee03bdec9da112171b
flx-lenovo-ubuntu <felixporzig@gmail.com>
2024-12-05 21:42:33 +0100

    working fine, norminette ready

--- ft_print_hexa.c
+++ ft_print_hexa.c
@@ -6,24 +6,24 @@
 /*   By: fporzig <fporzig@student.42berlin.de>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 16:48:42 by fporzig           #+#    #+#             */
-/*   Updated: 2024/12/05 16:48:43 by fporzig          ###   ########.fr       */
+/*   Updated: 2024/12/05 21:36:07 by fporzig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int ft_print_hexa(unsigned int n, int uppercase)
+int	ft_print_hexa(unsigned int n, int uppercase)
 {
-    char *hex;
-    if (uppercase)
-        hex = "0123456789ABCDEF";
-    else
-        hex = "0123456789abcdef";
+	char	*hex;
+	int		count;
 
-    int count = 0;
-
-    if (n >= 16)
-        count += ft_print_hexa(n / 16, uppercase);
-    ft_print_char(hex[n % 16]);
-    return count + 1;
+	if (uppercase)
+		hex = "0123456789ABCDEF";
+	else
+		hex = "0123456789abcdef";
+	count = 0;
+	if (n >= 16)
+		count += ft_print_hexa(n / 16, uppercase);
+	ft_print_char(hex[n % 16]);
+	return (count + 1);
 }
